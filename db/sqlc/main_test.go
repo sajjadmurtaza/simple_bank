f115ad0838402acc90c5c0b412868ddb9b1f0638
Sajjad Murtaza <sajjadmurtaza.nxb@gmail.com>
2025-03-11 11:18:40 +0900

    add main test for db connection and set quries

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatalf("Cannot connect to DB: %v", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sajjadmurtaza/simple_bank
 
 go 1.24.0
+
+require github.com/lib/pq v1.10.9
